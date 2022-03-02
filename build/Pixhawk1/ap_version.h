5cbfb070a61c1f38541c19310f8ceeaf28607df8
Mateusz Hinca <mateuszh.konto@gmail.com>
2022-03-02 22:58:20 +0100

    build

---
+++ build/Pixhawk1/ap_version.h
@@ -0,0 +1,11 @@
+// auto-generated header, do not edit
+
+#pragma once
+
+#ifndef FORCE_VERSION_H_INCLUDE
+#error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
+#endif
+
+#define GIT_VERSION "a380ba5b"
+#define GIT_VERSION_INT 2743122523
+#define CHIBIOS_GIT_VERSION "93e6e03d"
---
+++ build/c4che/_cache.py
@@ -0,0 +1,7 @@
+AUTOCONFIG = True
+BINDIR = '/usr/bin'
+BOARD = 'Pixhawk1'
+COVERAGE = False
+DEBUG = False
+LIBDIR = '/usr/lib'
+PREFIX = '/usr'
