cbaf630a37a7a978a9bf71a861475e470ee8f556
Mateusz Hinca <mateuszh.konto@gmail.com>
2022-03-03 00:47:36 +0100

    test

--- build/Pixhawk1/ap_version.h
+++ build/Pixhawk1/ap_version.h
@@ -6,6 +6,6 @@
 #error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
 #endif
 
-#define GIT_VERSION "a380ba5b"
-#define GIT_VERSION_INT 2743122523
+#define GIT_VERSION "5cbfb070"
+#define GIT_VERSION_INT 1556066416
 #define CHIBIOS_GIT_VERSION "93e6e03d"
